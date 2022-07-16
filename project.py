bdfc6c2e4aab8ce5d8ccc81cd6b676c0d194157c
powcoder代写 <powcoder@163.com>
2022-07-16 11:10:59 +0800

    程序代写 加微信 powcoder Programming Help Add Wechat powcoder

---
+++ project.py
@@ -0,0 +1,64 @@
+"""
+    SOURCE HEADER GOES HERE!
+"""
+
+import itertools
+from operator import itemgetter
+
+def open_file():
+    """
+    Insert function header
+    """
+    pass
+
+def read_file(fp):
+    """
+    Insert function header
+    """
+    pass
+
+def calculate_score(rack,word):
+    """
+    Insert function header
+    """
+    pass
+
+def generate_combinations(rack,placed_tile):
+    """
+    Insert function header
+    """
+    pass
+
+def generate_words(combo,scrabble_words_dict):    
+    """
+    Insert function header
+    """
+    pass
+
+def generate_words_with_scores(rack,placed_tile,scrabble_words_dict):
+    """
+    Insert function header
+    """
+    pass
+
+def sort_words(word_dic):
+    """
+    Insert function header
+    """
+    pass
+
+def display_words(word_list,specifier):
+    """
+    Insert function header
+    """
+    pass
+
+def main():
+    """
+    Insert function header
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
+
